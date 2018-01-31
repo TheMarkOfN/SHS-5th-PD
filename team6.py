5f76dd6b2d2e053578f38a3a3a307b18fc65286a
Avery Schaefer <220566@shakopeeschools.org>
2018-01-31 12:44:39 -0600

    Alternate strategies 1-31-18

--- team6.py
+++ team6.py
@@ -34,6 +34,45 @@ def move(my_history, their_history, my_score, their_score):
         return('b')
     else:
         if len(their_history)<10:
+            if 'b' == their_history[-1]:
+                return('b')
+            else:
+                if len(their_history) == 0:
+                    return('c')
+                else:
+                    return('c')
+        else:
+            for i in their_history[-1:-10]:
+                if i is 'b': #if a syntax, then replace is with =
+                    betrays = betrays + 1
+            if betrays > 7.5:
+                if random.randint(1,10) < 9.5:
+                    return('b')
+                else:
+                    return('c')
+            if betrays > 5.5 and betrays < 7.5:
+                if random.randint(1,10) < 7.5:
+                    return('b')
+                else:
+                    return('c')
+            if betrays < 5.5:
+                if random.randint(1,10) > 4.5:
+                    return('b')
+                else:
+                    return('c')
+                
+def test_move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+    betrays = 0
+    if not 'b' or 'c' in their_history:
+        return('b')
+    else:
+        if len(their_history)<10:
             if 'b' in their_history:
                 return('b')
             else:
@@ -57,4 +96,42 @@ def move(my_history, their_history, my_score, their_score):
                     return('b')
                 else:
                     return('c')
-                
+                    
+def test_move_2(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+    betrays = 0
+    if not 'b' or 'c' in their_history:
+        return('b')
+    else:
+        if len(their_history)<10:
+            if 'bbb' in their_history:
+                return('b')
+            else:
+                if random.randint(1,10) > 2.5:
+                    return('c')
+                else:
+                    return('b')
+        else:
+            for i in their_history[-1:-10]:
+                if i is 'b': #if a syntax, then replace is with =
+                    betrays = betrays + 1
+            if betrays > 7.5:
+                if random.randint(1,10) < 9.5:
+                    return('b')
+                else:
+                    return('c')
+            if betrays > 5.5 and betrays < 7.5:
+                if random.randint(1,10) < 7.5:
+                    return('b')
+                else:
+                    return('c')
+            if betrays < 5.5:
+                if random.randint(1,10) > 4.5:
+                    return('b')
+                else:
+                    return('c')
