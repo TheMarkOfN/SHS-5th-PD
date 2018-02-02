6981c3f68960d92dd190f659508d7cc4adc27abc
Avery Schaefer <220566@shakopeeschools.org>
2018-02-02 10:46:32 -0600

    New move

--- team6.py
+++ team6.py
@@ -148,4 +148,35 @@ def test_move(my_history, their_history, my_score, their_score):
                         return('b')
                     else:
                         return('c')
-                
+
+def test_move_2(my_history, their_history, my_score, their_score):
+    '''Alternative move number 2'''
+    
+    betrays = 0
+    percent_betrayed = betrays/len(their_history)
+    if len(their_history) == 0:
+        return('b')
+    else:
+        for i in their_history:
+            if i == 'b':
+                betrays = betrays + 1
+        if percent_betrayed <= .25:
+            if random.randint(1,10) < 2.5:
+                return('b')
+            else:
+                return('c')
+        if percent_betrayed > .25 and percent_betrayed <= .5:
+            if random.randint(1,10) < 4.5:
+                return('b')
+            else:
+                return('c')
+        if percent_betrayed >.5 and percent_betrayed <=  .75:
+            if random.randint(1,10) > .45:
+                return('b')
+            else:
+                return('b')
+        if percent_betrayed > .75:
+            if random.randint > .25:
+                return('b')
+            else:
+                return('c')
