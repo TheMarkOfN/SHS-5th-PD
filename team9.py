4ca439fb68febf5c9fcdeacfb8ca62183d194fb4
Mitchell Nadeau <mitchell.nadeau@gmail.com>
2018-02-05 12:34:11 -0600

    Final Fix

--- team9.py
+++ team9.py
@@ -8,25 +8,9 @@
 import random
 
 team_name = 'DillyDilly' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'Savagery'
 strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
 
 
     
@@ -47,7 +31,7 @@ def test_move(my_history, their_history, my_score, their_score, result):
         return False
 
 def move(my_history, their_history, my_score, their_score):
-    if len(my_history)==0: # It's the first round; betray.
+    if len(my_history)==0: # It's the first round; collude.
             return 'c'
     else: 
         if 'b' in their_history[-1:]:
