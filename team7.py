663ccf74cec35e67c833b1d141edeef742712510
200675 <200675@shakopeeschools.org>
2018-02-06 12:40:25 -0600

    changes

--- team7.py
+++ team7.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Seven' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Betray until punished'
+strategy_description = 'Always betray unless first round, severely punished last 5 rounds, or they successfully colluded last round'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
